bae85311b3248727e44cd2c409a9c70dd3603d66
monda00 <monda0524@gmail.com>
2020-07-04 14:06:54 +0900

    add addTodo feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Form, InputGroup, Input, InputGroupAddon, Button } from 'reactstrap';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addTodo(value)
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, text]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="mt-4">
+        <h1>Todo List</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Input type="text"
+              value={value}
+              onChange={e => setValue(e.target.value)} />
+            <InputGroupAddon addonType="append">
+              <Button type="submit" color="primary">追加</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
     </div>
   );
 }
