9203af693fe6c1b3b46e9bba206d1e3f28924a23
monda00 <monda0524@gmail.com>
2020-07-07 12:47:39 +0900

    add TodoForm component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container, Form, InputGroup, Input, InputGroupAddon, Button, Table } from 'reactstrap';
+import { Container, Button, Table } from 'reactstrap';
+import TodoForm from './components/TodoForm';
 
 function App() {
-  const [value, setValue] = useState('')
   const [todos, setTodos] = useState([])
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    addTodo(value)
-    setValue('')
-  }
 
   const addTodo = text => {
     const newTodos = [...todos, {text, complete: false}]
@@ -35,20 +29,7 @@ function App() {
     <div className="App">
       <Container className="mt-4">
         <h1>Todo List</h1>
-        <Form onSubmit={handleSubmit}>
-          <InputGroup>
-            <Input
-              type="text"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            />
-            <InputGroupAddon addonType="append">
-              <Button type="submit" color="primary">
-                追加
-              </Button>
-            </InputGroupAddon>
-          </InputGroup>
-        </Form>
+        <TodoForm addTodo={addTodo} />
       </Container>
       <Container>
         <Table>
